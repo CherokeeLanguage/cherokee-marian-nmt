6b697c026272926666ade3ee89ded3093596ce52
mjoyner <mjoyner@vbservices.net>
2019-10-26 12:16:48 -0400

    CUDA
    Initial work to get CUDA ready docker image compiled.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-docker build -t mariannmt-20191014.01-1804 -f Dockerfile.1804 .
+set -e
+set -o pipefail
+
+docker build -t cuda-mariannmt-20191025.01-1804 -f Dockerfile-cuda.1804 .
+
+#docker build -t mariannmt-20191022.01-1804 -f Dockerfile-opencl.1804 .
+
+#docker build -t mariannmt-20191014.01-1804 -f Dockerfile.1804 .
 
 docker build -t mariannmt-20191014.01-1904 -f Dockerfile.1904 .
