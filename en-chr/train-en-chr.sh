426f67a9eda7a27cf58a443210e8fbb2fafb92a7
mjoyner <mjoyner@vbservices.net>
2019-11-01 21:02:55 -0400

    Factor out common code.

--- en-chr/train-en-chr.sh
+++ en-chr/train-en-chr.sh
@@ -6,9 +6,10 @@ set -o pipefail
 cd "$(dirname "$0")"
 cwd="$(pwd)"
 
-MARIAN=/git/marian
-
 EPOCHS=10000
+
+. ./en-chr.vars
+. ./en-chr.funcs
 
 export PATH="$PATH:$MARIAN/build/"
 
@@ -17,35 +18,6 @@ then
     echo "marian is not installed in $MARIAN/build, you need to compile the toolkit first"
     exit 1
 fi
-
-DOALIGN=1
-
-L1="en"
-L2="chr"
-
-WORKDIR="/data/work.$L1-$L2"
-MODELDIR="/data/model.$L1-$L2"
-TEMPDIR="/data/temp.$L1-$L2"
-CORPUS_SRC="/data/corpus.src"
-CORPUS_SRC_DEV="/data/corpus.src.dev"
-CORPUS="$WORKDIR/corpus"
-STARTINGCORPUS="$WORKDIR/starting-corpus"
-SYNTHETICCORPUS="$WORKDIR/synthetic-corpus"
-DEVCORPUS="$WORKDIR/dev-corpus"
-TESTCORPUS="$WORKDIR/test-corpus"
-MONODIR="/data/mono.src"
-L1COUNT=8192
-L2COUNT=8192
-
-function reformatAsSentences {
-    cat "$1" | dos2unix \
-    | perl -0 -C -lpe 's/\n([^\n])/ $1/g' \
-    | perl -0 -C -lpe 's/\n+\s*/\n/g' \
-    | perl -C -lpe 's/\0/\n/g' \
-    | perl -C -lpe 's/ +/ /g' \
-    | perl -C -lpe 's/([.?!;:])\s+/$1\n/g' \
-    | perl -C -lpe 's/_//g'
-}
 
 if [ ! -d "$CORPUS_SRC" ]; then echo "MISSING $CORPUS_SRC"; exit -1; fi
 
@@ -96,12 +68,6 @@ $MARIAN/build/spm_train --input="$MONODIR/corpus-sentencepiece.$L2" \
     --model_prefix="/data/model.spm/vocab.$L2.spm" \
     --vocab_size=$L2COUNT --character_coverage=1.0
 
-function wgetIfNeeded {
-    if [ -f "$1" ]; then return; fi
-    wget -N -O /tmp/wget-temp.$$.txt "$2"
-    mv -v "/tmp/wget-temp.$$.txt" "$1"
-}
-
 #Additional monolingual corpus for $L1 English
 wgetIfNeeded "$MONODIR/$L1/Frankenstien.$L1" 'https://www.gutenberg.org/files/84/84-0.txt'
 wgetIfNeeded "$MONODIR/$L1/Pride-and-Prejudice.$L1" 'https://www.gutenberg.org/files/1342/1342-0.txt'
@@ -132,79 +98,12 @@ mv -v "/data/model.spm/vocab.$L2.spm.model" "/data/model.spm/vocab.$L2.spm"
 #copy in pretrained models for SPM
 cp -v /data/model.spm/vocab.*.spm "$MODELDIR/"
 
-# calculate max word counts
-function getMaxWordCount {
-    cat $CORPUS.$1 | /git/marian/build/spm_encode --model "$MODELDIR/vocab.$1.spm" | wc -w -l | while read words lines; do
-        echo "$(($lines / $words))"
-        return
-    done
-}
-
 l1Maxwords=$(getMaxWordCount $L1)
 l2Maxwords=$(getMaxWordCount $L2)
 maxwords=$((($l1Maxwords+$l2Maxwords+2)*16))
 
 echo "$L1: $l1Maxwords maxwords, $L2: $l2Maxwords maxwords"
 echo "Mini batch maxwords: $maxwords"
-
-# create alignment helper to improve word associations between languages. (In theory.)
-# requires pretrained modles for SPM!
-function doAlign {
-    if [ $DOALIGN != 1 ]; then
-        ALIGN_ARGS=""
-        ALIGN_ARGS2=""
-        return
-    fi
-
-   	cat $CORPUS.$L1 | /git/marian/build/spm_encode --model "$MODELDIR/vocab.$L1.spm" > $TEMPDIR/align.temp.$L1
-	cat $CORPUS.$L2 | /git/marian/build/spm_encode --model "$MODELDIR/vocab.$L2.spm" > $TEMPDIR/align.temp.$L2
-    
-    ALIGNEDCORPUS="$WORKDIR"/corpus.align.$L1-$L2
-    ALIGN_ARGS="--guided-alignment $ALIGNEDCORPUS"
-    paste $TEMPDIR/align.temp.$L1 $TEMPDIR/align.temp.$L2 > $TEMPDIR/align.temp.$L1-$L2
-    sed -i 's/\t/ ||| /g' $TEMPDIR/align.temp.$L1-$L2
-	/git/fast_align/build/fast_align -vdo -i $TEMPDIR/align.temp.$L1-$L2 > $TEMPDIR/forward.align.$L1-$L2
-	/git/fast_align/build/fast_align -vdor -i $TEMPDIR/align.temp.$L1-$L2 > $TEMPDIR/reverse.align.$L1-$L2
-	/git/fast_align/build/atools -c grow-diag-final -i $TEMPDIR/forward.align.$L1-$L2 -j $TEMPDIR/reverse.align.$L1-$L2 > "$ALIGNEDCORPUS"
-    
-    ALIGNEDCORPUS2="$WORKDIR"/corpus.align.$L2-$L1
-    ALIGN_ARGS2="--guided-alignment $ALIGNEDCORPUS2"
-    paste $TEMPDIR/align.temp.$L2 $TEMPDIR/align.temp.$L1 > $TEMPDIR/align.temp.$L2-$L1
-    sed -i 's/\t/ ||| /g' $TEMPDIR/align.temp.$L2-$L1
-    /git/fast_align/build/fast_align -vdo -i $TEMPDIR/align.temp.$L2-$L1 > $TEMPDIR/forward.align.$L2-$L1
-	/git/fast_align/build/fast_align -vdor -i $TEMPDIR/align.temp.$L2-$L1 > $TEMPDIR/reverse.align.$L2-$L1
-	/git/fast_align/build/atools -c grow-diag-final -i $TEMPDIR/forward.align.$L2-$L1 -j $TEMPDIR/reverse.align.$L2-$L1 > "$ALIGNEDCORPUS2"
-}
-
-function translateEnToChr {
-    cat "$1" | $MARIAN/build/marian-decoder -c "$MODELDIR"/model-$L1-$L2.npz.decoder.yml \
-        -w 4096 --devices 0 -b 6 -n0.6
-}
-
-function translateChrToEn {
-    cat "$1" | $MARIAN/build/marian-decoder -c "$MODELDIR"/model-$L2-$L1.npz.decoder.yml \
-        -w 4096 --devices 0 -b 6 -n0.6
-}
-
-function corpusSplit {
-    rm -v $TEMPDIR/corpus-?? 2> /dev/null || true
-    split -n l/10 -e "$1" "$TEMPDIR/corpus-"
-    cp /dev/null "$TEMPDIR/train-corpus.$L1-$L2.tsv"
-    y=0
-    for z in aa ab ac ad ae af ag ah ai aj; do
-        if [ "$y" = "$2" ]; then
-            cp "$TEMPDIR/corpus-$z" "$DEVCORPUS.$L1-$L2.tsv"
-            cut -f 1 "$DEVCORPUS.$L1-$L2.tsv" > "$DEVCORPUS.$L1"
-            cut -f 2 "$DEVCORPUS.$L1-$L2.tsv" > "$DEVCORPUS.$L2"
-        else
-            cat "$TEMPDIR/corpus-$z" >> "$TEMPDIR/train-corpus.$L1-$L2.tsv"
-        fi 
-        y="$(($y + 1))"
-    done
-    cut -f 1 "$TEMPDIR/train-corpus.$L1-$L2.tsv" > "$CORPUS.$L1"
-    cut -f 2 "$TEMPDIR/train-corpus.$L1-$L2.tsv" > "$CORPUS.$L2"
-}
-
 
 for loops in $(seq 1 1 1); do
 
@@ -237,6 +136,11 @@ for s in $(seq 1 1 10); do
 
     doAlign
 
+    # reset stalled count before running next training cycle
+    if [ -f "$MODELDIR/model-$L1-$L2.npz.progress.yml" ]; then
+        sed -i 's/^stalled: .*$/stalled: 0/g' "$MODELDIR/model-$L1-$L2.npz.progress.yml"
+    fi
+
     # train nmt model - forwards
     nice $MARIAN/build/marian \
         --after-epochs $EPOCHS \
@@ -260,21 +164,20 @@ for s in $(seq 1 1 10); do
         --seed 1111 --exponential-smoothing \
         --normalize=0.6 --beam-size=6 --quiet-translation \
         $ALIGN_ARGS \
-        --valid-translation-output "$TEMPDIR/validation-translation-output.txt" \
-        --lr-decay-strategy stalled --lr-decay-start 1 --lr-report
-
+        --valid-translation-output "$TEMPDIR/validation-translation-output.txt" # \
+        #--lr-decay-strategy stalled --lr-decay-start 1 --lr-report
 done 
 
 #generate synthetic corpus en->chr
-BOOKLIST="Frankenstien Pride-and-Prejudice Moby-Dick Dr-Jekyll Sherlock-Holmes Dracula Grimms-Fairy-Tales Jungle-Book Jungle-Book-2"
-#BOOKLIST="Jungle-Book"
+#BOOKLIST="Frankenstien Pride-and-Prejudice Moby-Dick Dr-Jekyll Sherlock-Holmes Dracula Grimms-Fairy-Tales Jungle-Book Jungle-Book-2"
+BOOKLIST="Jungle-Book"
 cp /dev/null "$SYNTHETICCORPUS".$L1-$L2.tsv
 cp /dev/null "$TEMPDIR/combined-books.$L1"
 for book in $BOOKLIST; do
     reformatAsSentences "$MONODIR/$L1/${book}.$L1" >> "$TEMPDIR/combined-books.$L1"
 done
 translateEnToChr "$TEMPDIR/combined-books.$L1" > "$TEMPDIR/combined-books.$L2"
-paste "$TEMPDIR/combined-books.$L1" > "$TEMPDIR/combined-books.$L2" > "$SYNTHETICCORPUS".$L1-$L2.tsv
+paste "$TEMPDIR/combined-books.$L1" "$TEMPDIR/combined-books.$L2" > "$SYNTHETICCORPUS".$L1-$L2.tsv
 
 #reset corpus
 cut -f 1 "$STARTINGCORPUS".$L1-$L2.tsv > "$CORPUS".$L1
@@ -292,6 +195,11 @@ rm "$MODELDIR"/model-$L2-$L1.npz.yml 2> /dev/null || true
 echo "$L2-$L1" >> "$TEMPDIR"/validation.log
 
 doAlign
+
+# reset stalled count before running next training cycle
+if [ -f "$MODELDIR/model-$L2-$L1.npz.progress.yml" ]; then
+    sed -i 's/^stalled: .*$/stalled: 0/g' "$MODELDIR/model-$L2-$L1.npz.progress.yml"
+fi
 
 # train nmt model - backwards
 nice $MARIAN/build/marian \
@@ -316,8 +224,8 @@ nice $MARIAN/build/marian \
     --seed 1111 --exponential-smoothing \
     --normalize=0.6 --beam-size=6 --quiet-translation \
     $ALIGN_ARGS \
-    --valid-translation-output "$TEMPDIR/validation-translation-output.txt" \
-    --lr-decay-strategy stalled --lr-decay-start 1 --lr-report
+    --valid-translation-output "$TEMPDIR/validation-translation-output.txt" #\
+    #--lr-decay-strategy stalled --lr-decay-start 1 --lr-report
 
 #generate synthetic corpus chr->en
 cut -f 2 "$SYNTHETICCORPUS".$L1-$L2.tsv > "$SYNTHETICCORPUS".$L2
